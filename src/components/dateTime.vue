d237f404c71390dc84f1bfb93b96c6efd1f38228
HXJ <1494362400@qq.com>
2021-08-08 15:02:49 +0800

    完善

--- src/components/dateTime.vue
+++ src/components/dateTime.vue
@@ -4,9 +4,9 @@
       v-model="timeData.timeSelect"
       v-for="(item, idx) in timeArr"
       :key="idx"
-      @change="selectTime(item)"
+      @change="selectTime(item.type)"
     >
-      <el-radio-button :label="item"></el-radio-button>
+      <el-radio-button :label="item.text"></el-radio-button>
     </el-radio-group>
 
     <el-date-picker
@@ -24,10 +24,32 @@
 export default {
   data() {
     return {
-      pickerOptions: {},
-      timeArr: ["昨日", "今日", "本周", "本月", "自定义"],
-      timeValue: "",
-      timeData: {
+      pickerOptions: {},//禁用
+      timeArr: [
+        {
+          text: "昨日",
+          type: 1,
+        },
+        {
+          text: "今日",
+          type: 2,
+        },
+        {
+          text: "本周",
+          type: 3,
+        },
+        {
+          text: "本月",
+          type: 4,
+        },
+        {
+          text: "自定义",
+          type: -1,
+        },
+      ],
+      todayNow: this.$moment().format(),
+      timeValue: "",//
+      timeData: {//传给父级的内容
         timeSelect: "今日",
         time: {
           start: "",
@@ -37,77 +59,90 @@ export default {
     };
   },
   methods: {
-    selectTime(val) {
-      var moment = require("moment");
+    //选项
+    selectTime(type) {
       this.timeValue = [];
-      if (val == "昨日") {
-        this.timeValue.push(
-          moment().subtract(1, "day").format("YYYY-MM-DD 00:00:00")
-        );
-        this.timeValue.push(
-          moment().subtract(1, "day").format("YYYY-MM-DD 23:59:59")
-        );
-        this.timeData.time.start = Date.parse(
-          moment().subtract(1, "day").format("YYYY-MM-DD 00:00:00")
-        );
-        this.timeData.time.end = Date.parse(
-          moment().subtract(1, "day").format("YYYY-MM-DD 23:59:59")
-        );
-        this.pickerOptions = {
-          disabledDate(time) {
-            return time.getTime()!=moment().subtract(1, "day");
-          },
-        };
-      } else if (val == "今日") {
-        this.timeValue.push(moment().format("YYYY-MM-DD 00:00:00"));
-        this.timeValue.push(moment().format("YYYY-MM-DD 23:59:59"));
-        this.timeData.time.start = Date.parse(
-          moment().format("YYYY-MM-DD 00:00:00")
-        );
-        this.timeData.time.end = Date.parse(
-          moment().format("YYYY-MM-DD 23:59:59")
-        );
-        this.pickerOptions = {
-          disabledDate(time) {
-            return time.getTime()!=moment();
-          },
-        };
-      } else if (val == "本周") {
-        this.timeValue.push(moment().day(0).format("YYYY-MM-DD 00:00:00"));
-        this.timeValue.push(moment().format("YYYY-MM-DD 23:59:59"));
-        this.timeData.time.start = Date.parse(
-          moment().day(0).format("YYYY-MM-DD 00:00:00")
-        );
-        this.timeData.time.end = Date.parse(
-          moment().format("YYYY-MM-DD 23:59:59")
-        );
-        this.pickerOptions = {
-          disabledDate(time) {
-            return time.getTime()<moment().day(0) || time.getTime()>moment();
-          },
-        };
-      } else if (val == "本月") {
-        this.timeValue.push(moment().date(1).format("YYYY-MM-DD 00:00:00"));
-        this.timeValue.push(moment().format("YYYY-MM-DD 23:59:59"));
-        this.timeData.time.start = Date.parse(
-          moment().date(1).format("YYYY-MM-DD 00:00:00")
-        );
-        this.timeData.time.end = Date.parse(
-          moment().format("YYYY-MM-DD 23:59:59")
-        );
-        this.pickerOptions = {
-          disabledDate(time) {
-            return time.getTime()<moment().date(1) || time.getTime()>moment();
-          },
-        };
+      switch (type) {
+        //昨天
+        case 1:
+          this.timeValue.push(
+            this.$moment().subtract(1, "day").format("YYYY-MM-DD 00:00:00")
+          );
+          this.timeValue.push(
+            this.$moment().subtract(1, "day").format("YYYY-MM-DD 23:59:59")
+          );
+          this.timeData.time.start = Date.parse(this.$moment()
+            .subtract(1, "day")
+            .format("YYYY-MM-DD 00:00:00"))
+          this.timeData.time.end =Date.parse( this.$moment()
+            .subtract(1, "day")
+            .format("YYYY-MM-DD 23:59:59"))
+          this.pickerOptions = {
+            disabledDate:(time)=>{
+              return time.getTime() != this.$moment().subtract(1, "day");
+            }
+          };
+          break;
+        //今天 
+        case 2:
+          this.timeValue.push(this.$moment().format("YYYY-MM-DD 00:00:00"));
+          this.timeValue.push(this.todayNow);
+          this.timeData.time.start = Date.parse(this.todayNow)
+
+          this.timeData.time.end = Date.parse(this.todayNow)
+          this.pickerOptions = {
+            disabledDate:(time)=> {
+              return time.getTime() != this.$moment();
+            },
+          };
+          break;
+          //本周
+        case 3:
+          this.timeValue.push(
+            this.$moment().day(0).format("YYYY-MM-DD 00:00:00")
+          );
+          this.timeValue.push(this.todayNow);
+          this.timeData.time.start = Date.parse(this.$moment()
+            .day(0)
+            .format("YYYY-MM-DD 00:00:00"))
+          this.timeData.time.end = Date.parse(this.todayNow)
+          this.pickerOptions = {
+            disabledDate:(time)=> {
+              return (
+                time.getTime() < this.$moment().day(0) ||
+                time.getTime() > this.$moment()
+              );
+            },
+          };
+          break;
+          //本月
+        case 4:
+          this.timeValue.push(
+            this.$moment().date(1).format("YYYY-MM-DD 00:00:00")
+          );
+          this.timeValue.push(this.todayNow);
+          this.timeData.time.start = Date.parse(this.$moment()
+            .date(1)
+            .format("YYYY-MM-DD 00:00:00"))
+          this.timeData.time.end = Date.parse(this.todayNow)
+          this.pickerOptions = {
+            disabledDate:(time)=> {
+              return (
+                time.getTime() < this.$moment().date(1) ||
+                time.getTime() > this.$moment()
+              );
+            },
+          };
+          break;
+        case -1:
+          this.pickerOptions = {
+            disabledDate() {
+              return false;
+            },
+          };
+          break;
       }
-      else{
-        this.pickerOptions = {
-          disabledDate() {
-            return false;
-          },
-        }
-      }
+      //传给父级
       this.$emit("timeParse", this.timeData);
     },
   },
--- src/main.js
+++ src/main.js
@@ -4,10 +4,15 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import VueRouter from 'vue-router'
 import router from './router/index'
+import moment from 'moment'
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 Vue.use(VueRouter)
+moment.locale("zh-cn")
+Vue.prototype.$moment=moment
 new Vue({
   render: h => h(App),
-  router
+  router,
 }).$mount('#app')
+
+
